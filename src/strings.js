4b2abbe71a561a1e71db35cb64df4fa3285f4dd0
Tom Hinkle <tmhinkle@gmail.com>
2020-04-04 01:51:08 -0400

    ++strings

---
+++ src/strings.js
@@ -0,0 +1,14 @@
+const lang =  {
+    steps : [
+        'Add words',
+        'Review words',
+        'Pull words from hat',
+        'Wait Turn',
+    ],
+};
+
+lang.addStep = lang.steps[0]
+lang.reviewStep = lang.steps[1]
+lang.pullStep = lang.steps[2]
+lang.wait = lang.steps[3]
+export default lang
