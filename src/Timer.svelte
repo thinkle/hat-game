b9d946fce194782c9e7c92e8fbdc06799cc2f7c8
Tom Hinkle <tmhinkle@gmail.com>
2020-04-05 09:08:53 -0400

    fancy up timer a bit

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -37,13 +37,27 @@
      }
      )
  }
+ function getProgressStyle () {
+     if (secondsTicked > timerLength) {
+         return "background-color:red;width:100%"
+     }
+     else {
+         return `background-color:green;width:${100*secondsTicked/timerLength}%`
+     }
+ }
 
 </script>
 <main>
     <div>
-        Tick... 
         {#if start}
-        <span>{Math.max(secondsTicked,0)} of {timerLength} seconds...</span>
+        <span>
+            <div class="total">
+                <div class="progress"
+                     style={getProgressStyle(secondsTicked)}
+                ></div>
+                <div class="text">{Math.max(secondsTicked,0)} of {timerLength} seconds...</div>
+            </div>
+        </span>
         {#if secondsTicked > timerLength}
         <b>{secondsTicked - timerLength} Over!</b>
         {/if}
@@ -57,4 +71,27 @@
 </main>
 <style>
  b {color: red}
+ .total {
+     height: 25px;
+     position: relative;
+     padding: 5px;
+     border: 1px solid black;
+     border-radius: 5px;
+     box-sizing: border-box;
+ }
+ .progress {
+     position: absolute;
+     top: 0;
+     left: 0;
+     height: 25px;
+ }
+ .text {
+     color: black;
+     font-family: futura;
+     font-weight: bold;
+     font-size: 16px;
+     -webkit-text-stroke: 1px white;
+     -webkit-text-fill-color: black;
+     position: relative;
+ }
 </style>
