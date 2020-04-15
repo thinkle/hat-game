2f1eda9b3678b2fea03f2715789248ac50b6d4ba
Tom Hinkle <tmhinkle@gmail.com>
2020-04-14 22:06:12 -0400

    play before review

--- src/strings.js
+++ src/strings.js
@@ -1,14 +1,14 @@
 const lang =  {
     steps : [
         'Add words',
-        'Review words',
         'Play',
+        'Review words',
     ],
 };
 
 lang.addStep = lang.steps[0]
-lang.reviewStep = lang.steps[1]
-lang.playStep = lang.steps[2]
+lang.reviewStep = lang.steps[2]
+lang.playStep = lang.steps[1]
 //lang.pullStep = lang.steps[2]
 //lang.wait = lang.steps[3]
 export default lang
